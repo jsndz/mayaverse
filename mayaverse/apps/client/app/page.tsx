4f8d6b3f461b78d6565e78a0aa2fbf9a1ecac457
jsndz <jaisondz9360@gmail.com>
2025-02-10 17:33:28 +0530

    client side

---
+++ mayaverse/apps/client/app/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from "@/components/AuthPage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <AuthForm></AuthForm>
+    </div>
+  );
+}
