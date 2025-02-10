e84af7e46f231ec634d9cc07c1cf6c790fa48473
jsndz <jaisondz9360@gmail.com>
2025-02-10 19:11:53 +0530

    cors

--- mayaverse/apps/http/src/config.ts
+++ mayaverse/apps/http/src/config.ts
@@ -3,3 +3,6 @@ dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const PORT = process.env.PORT;
+export const CLIENT_URL_PROD = process.env.CLIENT_URL_PROD;
+export const CLIENT_URL_DEV = process.env.CLIENT_URL_DEV;
+export const STATE = process.env.STATE;
--- mayaverse/apps/http/src/index.ts
+++ mayaverse/apps/http/src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import { router } from "./router/v1";
-import { PORT } from "./config";
+import { PORT, STATE, CLIENT_URL_PROD, CLIENT_URL_DEV } from "./config";
+import cors from "cors";
+
+let siteUrl = STATE === "development" ? CLIENT_URL_DEV : CLIENT_URL_PROD;
+const corsOptions = {
+  origin: siteUrl,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+};
+
 const app = express();
 app.use(express.json());
-app.use("/api/v1", router);
 
+app.use("/api/v1", router);
+app.use(cors());
 app.get("/", (req, res) => {
   res.json({
     message: "Hello",
